7eb1b3310ef38702e1098462e2f868413d1b2fb5
Louismousine <louisbarrettevanasse@yahoo.ca>
2020-07-08 00:55:54 -0400

    Selected font is now checked on popup opening

--- popup.js
+++ popup.js
@@ -4,6 +4,11 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 function changeFont(){
+    chrome.storage.sync.get(['key'], function(result) {
+        let font=result.key;
+        let checkedRadioButton = document.getElementById(font ? font : 'Arial');
+        checkedRadioButton.checked=true;
+    });
     let button = document.getElementById('button');
     button.addEventListener("click", function() {
         const font = document.querySelector('input[name="font"]:checked').value;
